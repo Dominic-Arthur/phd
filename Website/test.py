2bf61e5d8f9312df22421e878a6dc81f6d890dd5
Dominic Arthur <arthurdominic04@gmail.com>
2024-08-12 13:34:31 +0100

    Update test.py

--- Website/test.py
+++ Website/test.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from pathlib import Path
 
-st.image("IMG_0701.JPG", use_column_width=True,
+photo = Path(__file__).parents[1] / "IMG_0701.JPG"
+
+st.image(photo, use_column_width=True,
          caption="This is the second image caption, providing details about the photo.")
