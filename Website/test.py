34e17ea7345de4445e676b35fc6a3e74ef9fde7a
Dominic Arthur <arthurdominic04@gmail.com>
2024-08-12 13:53:58 +0100

    Update test.py

--- Website/test.py
+++ Website/test.py
@@ -1,13 +1,7 @@
 import streamlit as st
-from pathlib import Path
 import os
-
-os.getcwd()
 
 st.write(os.getcwd())
 
-photo = Path(__file__).parents[1] / "IMG_0701.JPG"
-
-st.write(photo)
-st.image("/mount/src/phd/IMG_0701.JPG", use_column_width=True,
+st.image("./Website/Photos/IMG_0701.JPG", use_column_width=True,
          caption="This is the second image caption, providing details about the photo.")
