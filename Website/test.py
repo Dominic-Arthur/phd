c889f7c24b4c04b204a863128dbaa005a62f1552
Dominic Arthur <arthurdominic04@gmail.com>
2024-08-12 13:38:17 +0100

    Update test.py

--- Website/test.py
+++ Website/test.py
@@ -3,5 +3,6 @@ from pathlib import Path
 
 photo = Path(__file__).parents[1] / "IMG_0701.JPG"
 
-st.image(photo, use_column_width=True,
-         caption="This is the second image caption, providing details about the photo.")
+st.write(photo)
+#st.image(photo, use_column_width=True,
+         #caption="This is the second image caption, providing details about the photo.")
