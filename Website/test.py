5840ebe3d816c98a9925b31935be91dfbce0aceb
Dominic Arthur <arthurdominic04@gmail.com>
2024-08-12 13:01:45 +0100

    Update test.py

--- Website/test.py
+++ Website/test.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.image("Photos/IMG_0701.JPG", use_column_width=True,
+st.image("./Photos/IMG_0701.JPG", use_column_width=True,
          caption="This is the second image caption, providing details about the photo.")
